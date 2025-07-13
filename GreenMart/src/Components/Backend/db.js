900fe1555d7d156fbbbc55c402c0733fec0a871e
Pranjal  Salunke <159419975+pranjalsalunke30@users.noreply.github.com>
2025-07-13 15:16:34 +0530

    Update db.js

--- GreenMart/src/Components/Backend/db.js
+++ GreenMart/src/Components/Backend/db.js
@@ -1,10 +1,11 @@
 const mysql = require("mysql");
 
 const db = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "pranjal@123",
-  database: "greenmart1",
+ host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
 });
 
 db.connect((err) => {
